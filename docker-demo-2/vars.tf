556ecf7d7d016af31e22df984c4dac3897736aa4
ashfakahmed2018 <38567677+ashfakahmed2018@users.noreply.github.com>
2019-07-09 21:05:53 -0400

    Add files via upload

--- docker-demo-2/vars.tf
+++ docker-demo-2/vars.tf
@@ -19,19 +19,3 @@ variable "ECS_AMIS" {
   }
 }
 # Full List: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
-
-variable "AMIS" {
-  type = "map"
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
-  }
-}
-variable "INSTANCE_DEVICE_NAME" {
-  default = "/dev/xvdh"
-}
-variable "JENKINS_VERSION" {
-  default = "2.73.2"
-}
-
--- docker-demo-3/s3.tf
+++ docker-demo-3/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform-state" {
-    bucket = "terraform-state-at6t7"
+    bucket = "terraform-state-a2b6219"
     acl = "private"
 
     tags {
