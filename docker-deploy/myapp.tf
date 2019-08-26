5418d4903ed77b97a920d69bd97e126d1366a248
ashfakahmed2018 <38567677+ashfakahmed2018@users.noreply.github.com>
2019-08-26 04:01:17 -0400

    Update myapp.tf

--- docker-deploy/myapp.tf
+++ docker-deploy/myapp.tf
@@ -2,8 +2,9 @@
 
 data "template_file" "myapp-task-definition-template" {
   template               = "${file("templates/app.json.tpl")}"
-  vars = {
+  vars {
     REPOSITORY_URL = "${replace("${aws_ecr_repository.myapp.repository_url}", "https://", "")}"
+    APP_VERSION = "${var.MYAPP_VERSION}"
   }
 }
 
@@ -12,6 +13,24 @@ resource "aws_ecs_task_definition" "myapp-task-definition" {
   container_definitions = "${data.template_file.myapp-task-definition-template.rendered}"
 }
 
+resource "aws_ecs_service" "myapp-service" {
+  count = "${var.MYAPP_SERVICE_ENABLE}"
+  name = "myapp"
+  cluster = "${aws_ecs_cluster.example-cluster.id}"
+  task_definition = "${aws_ecs_task_definition.myapp-task-definition.arn}"
+  desired_count = 1
+  iam_role = "${aws_iam_role.ecs-service-role.arn}"
+  depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
+
+  load_balancer {
+    elb_name = "${aws_elb.myapp-elb.name}"
+    container_name = "myapp"
+    container_port = 3000
+  }
+  lifecycle { ignore_changes = ["task_definition"] }
+}
+
+# load balancer
 resource "aws_elb" "myapp-elb" {
   name = "myapp-elb"
 
@@ -38,24 +57,8 @@ resource "aws_elb" "myapp-elb" {
   subnets = ["${aws_subnet.main-public-1.id}","${aws_subnet.main-public-2.id}"]
   security_groups = ["${aws_security_group.myapp-elb-securitygroup.id}"]
 
-  tags = {
+  tags {
     Name = "myapp-elb"
   }
-}
-
-resource "aws_ecs_service" "myapp-service" {
-  name = "myapp"
-  cluster = "${aws_ecs_cluster.example-cluster.id}"
-  task_definition = "${aws_ecs_task_definition.myapp-task-definition.arn}"
-  desired_count = 1
-  iam_role = "${aws_iam_role.ecs-service-role.arn}"
-  depends_on = ["aws_iam_policy_attachment.ecs-service-attach1"]
-
-  load_balancer {
-    elb_name = "${aws_elb.myapp-elb.name}"
-    container_name = "myapp"
-    container_port = 3000
-  }
-  lifecycle { ignore_changes = ["task_definition"] }
 }
 
